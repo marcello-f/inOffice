8ed1a89aab6d0978721b6a4aa90a314b52348d01
marcello-f <fredericks.marcello@gmail.com>
2022-11-12 21:42:45 +0000

    login and registration complete

--- inOffice/__init__.py
+++ inOffice/__init__.py
@@ -1,13 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "bigsecret"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    db.init_app(app)
 
     from .views import views
-    # from .auth import auth
+    from .auth import auth
+    
+    with app.app_context():
+        db.create_all()
+        print('DB created.')
 
     app.register_blueprint(views, url_prefix='/')
-    # app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
--- inOffice/auth.py
+++ inOffice/auth.py
@@ -1,15 +1,56 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        surname = request.form.get('surname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-@auth.route('/login')
+        user = User.query.filter_by(email=email).first()
+        if len(password1) < 4 or len(password2) < 4:  # type: ignore
+            flash('Passwords must be at least 4 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        else:
+            if user:
+                flash('An account associated with this email address already exists.', category='error')
+                return redirect(url_for('auth.signup'))
+            else:
+                new_user = User(email=email, firstname=firstname, surname=surname, password=generate_password_hash(password1, method='sha256'))
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember='True')
+                flash('Account has been successfully created!', category='success')
+                return redirect(url_for('views.home'))
+    return render_template('signup.html', user=current_user)
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember='True')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password. Please try again.', category="error") 
+        else:
+            flash('This email address is not associated with an account.', category="error") 
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for('auth.login'))
